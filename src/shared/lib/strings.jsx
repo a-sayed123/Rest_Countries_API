1970d903b000f688c3e7b47ce58de7c70c12adf8
Ahmed Sayed <a.marraey@gmail.com>
2026-09-18 18:55:08 +0300

    chore: finalize project metadata and CSP

--- src/shared/lib/strings.jsx
+++ src/shared/lib/strings.jsx
@@ -3,6 +3,6 @@ export function showCountryName(countryName){
     return countryName.length < 10 ? (
         <h2 className="country__name">{countryName}</h2>
     ) : (
-        <h2 className="country__name" style={{fontSize: "1.6rem"}}>{countryName}</h2>
+        <h2 className="country__name country__name--long">{countryName}</h2>
     )
 }
